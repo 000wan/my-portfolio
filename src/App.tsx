a3ccd248bdf8f16d40062729aeb0fac80c62d5a6
000wan <87213416+000wan@users.noreply.github.com>
2023-02-07 15:39:40 +0900

    Add prettier settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.css'
+import HomePage from "./pages/home";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
